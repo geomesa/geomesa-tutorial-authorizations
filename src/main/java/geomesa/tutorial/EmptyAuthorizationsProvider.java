a5df99bd7ddbfd06bc116528e75f03190cfe008f
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2014-06-06 11:18:55 -0400

    Tutorial project for querying data with authorizations

---
+++ src/main/java/geomesa/tutorial/EmptyAuthorizationsProvider.java
@@ -0,0 +1,22 @@
+package geomesa.tutorial;
+
+import geomesa.core.security.AuthorizationsProvider;
+import org.apache.accumulo.core.security.Authorizations;
+
+import java.io.Serializable;
+import java.util.Map;
+
+/**
+ * Implementation of auth provider that always returns empty auths
+ */
+public class EmptyAuthorizationsProvider implements AuthorizationsProvider {
+    @Override
+    public Authorizations getAuthorizations() {
+        return new Authorizations();
+    }
+
+    @Override
+    public void configure(Map<String, Serializable> params) {
+
+    }
+}
